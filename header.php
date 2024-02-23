64991e3813d81f6b55aeea70f0f5025dd840dc28
AhmadSOD <ahmadwasel06@gmail.com>
2024-02-23 19:41:11 +0100

    7

--- header.php
+++ header.php
@@ -1,15 +1,14 @@
 <header>
-<a href="index.php">
-    <img src="logo.jpg" alt="Scooter Webshop Logo" class="logo">
-</a>        <nav>
+<a href="index.php"> <img src="logo.jpg" alt="Scooter Webshop Logo" class="logo"></a>        
+    <nav>
             <ul>
                 <li class='h'><a href="index.php">Home</a></li>
                 <li><a href="producten.php">Producten</a></li>
                 <li><a href="inlog.php">klanten</a></li>
                 <li><a href="bestellingen.php">Bestellingen</a></li>
-                <li><a href="winkelwagen.php">winkelwagen</a></li>
                 <li><a href="over_ons.php">Over ons</a></li>
             
+
                 
                 
         <li class="dropdown">
@@ -18,10 +17,12 @@
                 <a href="producten.php">Producten</a>
                 <a href="inlog.php">Klanten</a>
                 <a href="bestellingen.php">Bestellingen</a>
-                <a href="winkelwagen.php">Winkelwagen</a>
                 <a href="over_ons.php">Over ons</a>
+              
             </div>
         </li>
+        <a href="winkelwagen.php"> <img src="winkelwagen.png" class="winkel"></a>
+
                 
             </ul>
             
